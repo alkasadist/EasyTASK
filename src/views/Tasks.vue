093d17791ed530bbdf17ce93358cfd1b5887806e
alkasadist <a1kasadist@ya.ru>
2025-06-04 17:17:32 +0300

    svg icons to Tasks.vue added

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -10,12 +10,15 @@
             </h3>
             <button
               @click="addTask"
-              class="text-white px-4 py-2 rounded 
+              class="flex items-center text-white px-4 py-2 rounded 
               bg-blue-600 hover:bg-blue-700">
-              Add task +
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+              </svg>
+              Add
             </button>
           </div>
-
+          
           <ul class="space-y-4">
             <!-- Pending Tasks -->
             <li
@@ -29,9 +32,11 @@
               <div class="flex space-x-2">
                 <button
                   @click="markAsDone(index)"
-                  class="text-white px-4 py-2 rounded 
-                  bg-green-600 hover:bg-green-700">
-                  Done
+                  class="p-2 rounded-md text-green-600 hover:bg-green-100"
+                  title="Mark as done">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                  </svg>
                 </button>
               </div>
             </li>
@@ -47,14 +52,19 @@
               <div class="flex space-x-2">
                 <button
                   @click="restoreTask(index)"
-                  class="text-white px-4 py-2 rounded 
-                  bg-blue-600 hover:bg-blue-700">
-                  Restore
+                  class="p-2 rounded-md text-blue-600 hover:bg-blue-100"
+                  title="Restore task">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+                  </svg>
                 </button>
                 <button
                   @click="deleteTask(index)"
-                  class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700">
-                  Delete
+                  class="p-2 rounded-md text-red-600 hover:bg-red-100"
+                  title="Delete task">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                  </svg>
                 </button>
               </div>
             </li>
